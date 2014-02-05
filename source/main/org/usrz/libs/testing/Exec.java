ad5aa93521d8cfadbf3c31a08a0ad4cc13ed68a2
Pier Fumagalli <pier@fumagalli.org>
2014-02-05 19:11:15 +0900

    Add support for capturing output of processes.

--- source/main/org/usrz/libs/testing/Exec.java
+++ source/main/org/usrz/libs/testing/Exec.java
@@ -15,6 +15,7 @@
  * ========================================================================== */
 package org.usrz.libs.testing;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -27,17 +28,140 @@ public final class Exec {
 
     public static final Process exec(String[] commandLine)
     throws IOException {
+        return exec(commandLine, System.out, System.err);
+    }
+
+    public static final Process exec(String[] commandLine, OutputStream systemOut, OutputStream systemErr)
+    throws IOException {
         StringBuilder builder = new StringBuilder("EXECUTING: ");
         for (String string: commandLine) builder.append(' ').append(string);
         System.out.println(builder.toString());
 
         final Process process = Runtime.getRuntime().exec(commandLine);
+        process.getOutputStream().close();
 
-        new Thread(new Copier(process.getInputStream(), System.out, "output")).start();
-        new Thread(new Copier(process.getErrorStream(), System.err, "output")).start();
+        final Thread copiers[] = new Thread[2];
+        copiers[0] = new Thread(new Copier(process.getInputStream(), systemOut, "output"));
+        copiers[1] = new Thread(new Copier(process.getErrorStream(), systemErr, "output"));
+        for (Thread copier: copiers) copier.start();
 
-        return process;
+        return new WaitingProcess(process, copiers);
     }
+
+    public static final CapturingProcess capture(String[] commandLine)
+    throws IOException {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final ByteArrayOutputStream error = new ByteArrayOutputStream();
+        return new CapturingProcess(exec(commandLine, output, error), output, error);
+    }
+
+    public static final class CapturingProcess extends Process {
+
+        private final Process process;
+        private final ByteArrayOutputStream output;
+        private final ByteArrayOutputStream error;
+
+        private CapturingProcess(Process process, ByteArrayOutputStream output, ByteArrayOutputStream error) {
+            this.process = process;
+            this.output = output;
+            this.error = error;
+        }
+
+        public byte[] getOutputBytes() {
+            return output.toByteArray();
+        }
+
+        public byte[] getErrorBytes() {
+            return error.toByteArray();
+        }
+
+        public String getOutput() {
+            return new String(output.toByteArray());
+        }
+
+        public String getError() {
+            return new String(error.toByteArray());
+        }
+
+        public OutputStream getOutputStream() {
+            throw new UnsupportedOperationException("Don't use!");
+        }
+
+        public InputStream getInputStream() {
+            throw new UnsupportedOperationException("Use \"getOutput()\"");
+        }
+
+        public InputStream getErrorStream() {
+            throw new UnsupportedOperationException("Use \"getError()\"");
+        }
+
+        public int waitFor() throws InterruptedException {
+            return process.waitFor();
+        }
+
+        public int exitValue() {
+            return process.exitValue();
+        }
+
+        public void destroy() {
+            process.destroy();
+        }
+    }
+
+    /* ====================================================================== */
+
+    private static final class WaitingProcess extends Process {
+
+        private final Process process;
+        private final Thread[] copiers;
+
+        private WaitingProcess(Process process, Thread[] copiers) {
+            this.process = process;
+            this.copiers = copiers;
+        }
+
+        public int waitFor()
+        throws InterruptedException {
+            for (Thread thread: copiers) {
+                thread.join();
+            }
+            return process.waitFor();
+        }
+
+        public int hashCode() {
+            return process.hashCode();
+        }
+
+        public OutputStream getOutputStream() {
+            return process.getOutputStream();
+        }
+
+        public InputStream getInputStream() {
+            return process.getInputStream();
+        }
+
+        public boolean equals(Object obj) {
+            return process.equals(obj);
+        }
+
+        public InputStream getErrorStream() {
+            return process.getErrorStream();
+        }
+
+        public int exitValue() {
+            return process.exitValue();
+        }
+
+        public void destroy() {
+            process.destroy();
+        }
+
+        public String toString() {
+            return process.toString();
+        }
+    }
+
+    /* ====================================================================== */
 
     private static final class Copier implements Runnable {
 
